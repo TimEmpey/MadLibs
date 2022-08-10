226e0d3e8097efdfea45c935804f754f0724da90
Tim Empey <timothyempey@gmail.com>
2022-08-10 10:12:33 -0700

    added madlibs code in js file
    co-authored-by: Shaniza Lingle <shanizalingle@gmail.com>
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>

---
+++ js/script.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    // gets value from form inputs
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput= document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    const nounInput = document.getElementById("nounInput").value;
+
+    // sets stroy variables from form
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
+
+    document.querySelector("div#story").removeAttribute("class");
+
+    event.preventDefault();
+  };
+};
